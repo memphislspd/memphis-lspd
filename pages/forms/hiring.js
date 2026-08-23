b6425ea092607731760f51d4d9d9ef53de792e67
memphislspd <akizarit02@gmail.com>
2026-08-23 18:25:41 +0300

    Update hiring.js

--- pages/forms/hiring.js
+++ pages/forms/hiring.js
@@ -115,7 +115,7 @@ export default function HiringForm() {
       </button>
       
       <div className="form-container">
-        <h1>📝 Трудоустройство в LSCSD</h1>
+        <h1>📝 Трудоустройство в lspd</h1>
         
         <form onSubmit={handleSubmit}>
           <div className="form-group">
